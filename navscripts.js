1e81bd606e41d35e19561a0e332a893237ebf3a7
qetennyson <quincytennyson@yahoo.com>
2016-11-26 09:30:33 -0500

    First commit for portfolio

---
+++ navscripts.js
@@ -0,0 +1,21 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+function openSubNav() {
+    document.getElementById("mySubnav").style.width = "250px";
+    document.getElementById("mySubnav").style.height = "250px";
+}
+
+function closeSubNav() {
+   document.getElementById("mySubnav").style.width = "0";
+   
+}
